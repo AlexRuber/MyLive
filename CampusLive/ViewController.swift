f9b995ad29c4c89f5950ea2a095a7d8b3841d553
Mihai Ruber <mihairuber@iulianas-mbp>
2017-01-21 21:23:23 -0800

    First Commit!

--- CampusLive/ViewController.swift
+++ CampusLive/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("first line of code")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
