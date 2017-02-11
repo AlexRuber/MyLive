56b1f78c778941b016dd9be24162e3382f6d6fbb
Raghav Nyati <raghavnyati90@gmail.com>
2017-02-11 02:09:09 -0800

    Database Integration for Events. + Pins on Map on Home Screen.

--- CampusLive/AddEventViewController.swift
+++ CampusLive/AddEventViewController.swift
@@ -8,10 +8,43 @@
 
 import UIKit
 import MapKit
+import Firebase
 
 class AddEventViewController: UIViewController {
 
     var location: CLLocation!
+    
+    let userRef = FIRDatabase.database().reference().child("event")
+    var uid: String?
+    
+    @IBOutlet weak var venueTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBAction func postButtonClicked(_ sender: Any) {
+        if FIRAuth.auth()?.currentUser != nil{
+            // User is signed in.
+            self.uid = FIRAuth.auth()?.currentUser?.uid
+            self.post()
+        }
+    }
+    
+    func post(){
+        
+        let name = nameTextField.text
+        let venue = venueTextField.text
+        let description = descriptionTextView.text
+        let startDate = String(describing: startDatePicker.date)
+        let endDate = String(describing: endDatePicker.date)
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        let posts: [String : AnyObject] = ["uid":uid as AnyObject, "name":name as AnyObject, "venue":venue as AnyObject, "description":description as AnyObject, "startDate":startDate as AnyObject, "endDate":endDate as AnyObject, "latitude":latitude as AnyObject, "longitude":longitude as AnyObject]
+        userRef.childByAutoId().setValue(posts)
+        print("Adding an event successfully.")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +56,4 @@ class AddEventViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-
 }
--- CampusLive/LiveViewController.swift
+++ CampusLive/LiveViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Firebase
+
+@objc class CampusLiveAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(lat: CLLocationDegrees, long: CLLocationDegrees, title: String? = nil, subtitle: String? = nil) {
+        self.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
 
 class LiveViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     
     @IBOutlet weak var mapView: MKMapView!
     
-    
     var location: CLLocation!
-    var addEventLocation: CLLocation!
+    var addEventLocation: CLLocation! = CLLocation(latitude: 27.8812, longitude: -123.2374)
     let locationManager = CLLocationManager()
     //Change value to false
     var isOrgLogin: Bool = false
@@ -26,6 +38,8 @@ class LiveViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     @IBOutlet weak var currentLocationButton: UIButton!
     @IBOutlet weak var eventPin: UIImageView!
     @IBOutlet weak var eventDescription: UIButton!
+    
+    let eventRef = FIRDatabase.database().reference().child("event")
     
     @IBAction func addEventDescription(_ sender: Any) {
         performSegue(withIdentifier: "AddEventDescription", sender: nil)
@@ -56,7 +70,31 @@ class LiveViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         
         //var gesture = UIPanGestureRecognizer(target: self, action: Selector("userDragged:"))
         //addEventButton.addGestureRecognizer(gesture)
-        
+        displayLiveEvents()
+    }
+    
+    func displayLiveEvents(){
+        //var episode:Episode? = nil
+        eventRef.observe(.value, with: {(snap) in
+            if let userDict = snap.value as? [String:AnyObject]{
+                
+                print(userDict)
+                for each in userDict as [String: AnyObject] {
+                    let autoID = each.key
+                    var name = each.value["name"] as! String
+                    var endDate = each.value["endDate"] as! String
+                    var venue = each.value["venue"] as! String
+                    var latitude = each.value["latitude"] as! NSNumber
+                    var longitude = each.value["longitude"] as! NSNumber
+                    var description = each.value["description"] as! String
+                    let annotation = MKPointAnnotation()
+                    annotation.title = name
+                    annotation.subtitle = venue
+                    annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        })
     }
     
     @IBAction func addEventButtonClicked(_ sender: Any) {
@@ -70,8 +108,6 @@ class LiveViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         // Dispose of any resources that can be recreated.
     }
     
-// MARK: - Location Delegate Methods 
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         let location = locations.last
@@ -80,7 +116,6 @@ class LiveViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
         self.mapView.setRegion(region, animated: true)
         self.locationManager.stopUpdatingLocation()
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -94,4 +129,46 @@ class LiveViewController: UIViewController, MKMapViewDelegate, CLLocationManager
             // destinationViewController.isOrgLogin = true
         }
     }
+    /*
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        /*
+        guard !annotation.isKind(of: MKUserLocation()) else {
+            return nil
+        }*/
+        /*if annotation.isKindOfClass(MKUserLocation){
+         //emty return, guard wasn't cooperating
+         }else{
+         return nil
+         }*/
+        
+        guard !annotation.isKind(of: MKUserLocation.self) else {
+            return nil
+        }
+        
+        let annotationIdentifier = "AnnotationIdentifier"
+        var annotationView: MKAnnotationView?
+        
+        if let dequeuedAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) {
+            annotationView = dequeuedAnnotationView
+            annotationView?.annotation = annotation
+        }
+        else {
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = av
+            annotationView?.isEnabled = true
+        }
+        
+        if let annotationView = annotationView {
+            // Configure your annotation view here
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "mapPins")
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: 22, height: 22)
+            btn.setImage(UIImage(named: "info"), for: UIControlState())
+            annotationView.rightCalloutAccessoryView = btn
+            
+        }
+        return annotationView
+    }*/
 }
